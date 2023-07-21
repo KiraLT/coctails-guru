a90f9823188cb7b2f3bb05b5b53da0b183170b29
KiraLT <KiraLT@users.noreply.github.com>
2023-07-21 12:14:27 +0300

    Sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+    output: 'export',
     siteUrl: 'https://cocktailsguru.me',
     generateRobotsTxt: true,
     generateIndexSitemap: false
