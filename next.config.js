bdadd3b31aba5d2b148ff93e9129c6569d99f774
KiraLT <KiraLT@users.noreply.github.com>
2023-11-11 01:35:25 +0200

    Add caching on front-end navigation

--- next.config.js
+++ next.config.js
@@ -2,15 +2,10 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
 })
-
 const withExportImages = require('next-export-optimize-images')
-
 const withYaml = require('next-plugin-yaml')
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+module.exports = withPWA(withYaml(withExportImages({
     reactStrictMode: false,
     output: 'export',
     trailingSlash: true,
@@ -18,6 +13,5 @@ const nextConfig = {
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
-}
-
-module.exports = withPWA(withYaml(withExportImages(nextConfig)))
+    cacheOnFrontEndNav: true,
+})))
