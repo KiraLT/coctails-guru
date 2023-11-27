561744e450cd0c0427755a930c3e81bfee955169
KiraLT <KiraLT@users.noreply.github.com>
2023-11-27 20:54:53 +0200

    Add scaling functionality to Ingredients component

--- src/components/recipe/ingredients.tsx
+++ src/components/recipe/ingredients.tsx
@@ -1,6 +1,9 @@
-import { getIngredientBySlug } from '@/controllers/ingredients'
+'use client'
+import { getIngredientBySlug, parseQuantity, stringifyQuantity } from '@/controllers/ingredients'
 import { titleCase } from 'common-stuff'
 import Link from 'next/link'
+import { useState } from 'react'
+import { Join, Button } from 'react-daisyui'
 
 export function Ingredients({
     ingredients,
@@ -9,13 +12,27 @@ export function Ingredients({
     ingredients: Record<string, string>
     className?: string
 }): JSX.Element {
+    const [scale, setScale] = useState<1 | 2 | 3>(1)
+
     return (
         <section className={`prose ${className ?? ''}`}>
-            <h3>Ingredients</h3>
+            <h3 className="flex justify-between">
+                <div>Ingredients</div>
+                <div>
+                    <Join>
+                        <Button size='sm' className="join-item" active={scale === 1} onClick={() => setScale(1)}>1</Button>
+                        <Button size='sm' className="join-item" active={scale === 2} onClick={() => setScale(2)}>2</Button>
+                        <Button size='sm' className="join-item" active={scale === 3} onClick={() => setScale(3)}>3</Button>
+                    </Join>
+                </div>
+            </h3> 
+            
             <ul className="list-none p-0">
                 {Object.entries(ingredients).map(([slug, quantity]) => {
                     const ingredient = getIngredientBySlug(slug)
+                    const [quantityValue, quantityUnits] = parseQuantity(quantity)
 
+                    const formattedQuantity = stringifyQuantity(quantityValue * scale, quantityUnits)
                     return (
                         <li key={slug}>
                             <label className="flex items-center">
@@ -25,7 +42,7 @@ export function Ingredients({
                                 />
                                 {ingredient ? (
                                     <>
-                                        {quantity}
+                                        {formattedQuantity}
                                         <Link
                                             href={`/ingredients/${slug}`}
                                             className="ml-1"
@@ -35,7 +52,7 @@ export function Ingredients({
                                     </>
                                 ) : (
                                     <>
-                                        {`${quantity} ${titleCase(
+                                        {`${formattedQuantity} ${titleCase(
                                             slug.split('-').join(' '),
                                         )}`}
                                     </>
--- src/controllers/ingredients/index.ts
+++ src/controllers/ingredients/index.ts
@@ -2,10 +2,51 @@ import { ingredients } from './loader'
 import { Ingredient } from './schema'
 export type { Ingredient }
 
+const specialNumbers: Record<string, number> = {
+    '½': 0.5,
+    '¼': 0.25,
+    '¾': 0.75,
+    '⅓': 0.33,
+    '⅔': 0.66,
+    '⅛': 0.125,
+    '⅜': 0.375,
+    '⅝': 0.625,
+    '⅞': 0.875,
+}
+
 export function getIngredientBySlug(slug: string): Ingredient | undefined {
     return ingredients.find((recipe) => recipe.slug === slug)
 }
 
 export function getAllIngredients(): Ingredient[] {
     return ingredients
+}
+export function parseQuantity(quantity: string): [number, string] {
+    const [amount, ...parts] = quantity.split(' ')
+    const amountLastChar = amount[amount.length - 1]
+
+    if (parts[0] in specialNumbers) {
+        return [
+            (parseFloat(amount) || 0) + specialNumbers[parts[0]],
+            parts.slice(1).join(' ')
+        ]
+    } else if (amountLastChar in specialNumbers) {
+        return [
+            (parseFloat(amount.slice(0, -1)) || 0) + specialNumbers[amountLastChar],
+            parts.join(' ')
+        ]
+    }
+    return [parseFloat(amount), parts.join(' ')]
+}
+
+export function stringifyQuantity(amount: number, unit: string): string {
+    const digits = amount % 1
+    const rounded = Math.trunc(amount)
+
+    const specialNumber = Object.entries(specialNumbers).find(([, value]) => value === digits)
+    if (specialNumber) {
+        return `${rounded || ''} ${specialNumber[0]} ${unit}`
+    }
+    
+    return `${amount} ${unit}`
 }
